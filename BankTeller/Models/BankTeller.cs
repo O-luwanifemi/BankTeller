b41297962db01cff5f0adea74a55a3419c9a10f9
Nifemi <rashid.oluwaseun@yahoo.com>
2021-11-03 08:50:02 -0700

    Added problem solution

---
+++ BankTeller/Models/BankTeller.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTellerApp.Models
+{
+    public class BankTeller
+    {
+        public string ShowCummulativeInterest()
+        {
+            double vat;
+            string response;
+            float interestRate;
+            double cummulativeInterest;
+            uint[] savingDurations = { 6, 9, 12, 24, 60 };
+
+            Console.WriteLine("Input your account name: ");
+            string accountName = Console.ReadLine();
+
+            Console.WriteLine("Input your account number: ");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Specify your account type: ");
+            string accountType = Console.ReadLine();
+
+            Console.WriteLine("Input an amount: ");
+            int principal = int.Parse(Console.ReadLine());
+
+            switch (accountType)
+            {
+                case "Saving":
+                    interestRate = 1.2F;
+                    break;
+                case "Current":
+                    interestRate = 3.9F;
+                    break;
+                case "Domiciliary":
+                    interestRate = 3.9F;
+                    break;
+                case "Corporate":
+                    interestRate = 3.9F;
+                    break;
+                default:
+                    interestRate = 5.5F;
+                    break;
+            }
+
+            vat = principal * (7.5 / 100);
+            foreach (var duration in savingDurations)
+            {
+                cummulativeInterest = principal * (1 + (interestRate / duration));
+                response = $"In {duration} months, you will have {cummulativeInterest} interests.";
+                Console.WriteLine(response);
+            }
+
+            return "";
+        }
+    }
+}
---
+++ BankTeller/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BankTellerApp.Models;
+
+namespace BankTellerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankTeller wemaTeller = new BankTeller();
+            Console.WriteLine(wemaTeller.ShowCummulativeInterest());
+        }
+    }
+}
